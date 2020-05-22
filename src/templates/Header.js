dbea517502a643b10e616df6a9f5aebe3ca7e4d4
Frandipfer <francinagio@hotmail.com>
2020-05-22 16:16:49 -0400

    Creacion de template de personajes part 2

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
             <div class="Header-logo">
                 <h1>
                     <a href="/">
-                        100tifi.co 
+                        Scientist.101
                     </a> 
                 </h1> 
             </div>
